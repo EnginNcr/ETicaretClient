579d9243c4de5d544935582da28a1dced6e00e4d
EnginNcr <104725257+EnginNcr@users.noreply.github.com>
2023-04-14 14:14:05 +0300

    Add Component and Module

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '../admin/layout/layout.module';
+import { ComponentsModule } from './components/components.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ],
   exports:[
     LayoutModule
